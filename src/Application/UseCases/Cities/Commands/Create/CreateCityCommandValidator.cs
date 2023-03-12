802e5dfbc0108b8bdcbb829a61476af43605ef3e
santos-an <6472330+santos-an@users.noreply.github.com>
2023-03-12 18:03:05 +0000

    add remaining validators

---
+++ src/Application/UseCases/Cities/Commands/Create/CreateCityCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.Cities.Commands.Create;
+
+public class CreateCityCommandValidator : AbstractValidator<CreateCityCommand>
+{
+    public CreateCityCommandValidator()
+    {
+        RuleFor(x => x.Name).NotNull();
+        RuleFor(x => x.Name).NotEmpty();
+        
+        RuleFor(x => x.Country).NotNull();
+        RuleFor(x => x.Country).NotEmpty();
+
+        RuleFor(x => x.SubCountry).NotNull();
+        RuleFor(x => x.SubCountry).NotEmpty();
+        
+        RuleFor(x => x.GeonameId).NotNull();
+        RuleFor(x => x.GeonameId).NotEmpty();
+    }
+}
